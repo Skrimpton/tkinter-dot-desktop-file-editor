b30e0ed723e58f0b3c747586315fa80ce5e951eb
Skrimpton <64572787+Skrimpton@users.noreply.github.com>
2024-02-26 22:13:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-#!/bin/env python3
+#!/bin/env python
 
 import sys,argparse,configparser,signal
 
+from subprocess import ( call as subprocess_call,
+                        DEVNULL
+)
+# from subprocess import DEVNULL
+
 try:
     import tkinter as tk
-    import tkinter.ttk as ttk
+    from tkinter import ttk,messagebox
     from tkinter.constants import *
 except:
     print("no tkinter, no joy"),sys.exit(1)
@@ -93,19 +98,19 @@ class VerticalScrolledFrame(ttk.Frame): # https://coderslegacy.com/python/make-s
     def __init__(self, parent, *args, **kw):
         ttk.Frame.__init__(self, parent, *args, **kw)
         self.grid_columnconfigure(0,weight=1)
-        self.grid_rowconfigure(0,weight=1)
+        self.grid_rowconfigure(1,weight=1)
+        # self.okbutton = ttk.Button(self,text="Save",command=self.ok_pressed)
+        # self.okbutton.grid(row=0,column=0,columnspan=2,sticky="we",pady=1,padx=1)
         # Create a canvas object and a vertical scrollbar for scrolling it.
         vscrollbar = ttk.Scrollbar(self, orient=VERTICAL)
-        vscrollbar.grid(row=0,column=1,sticky="ns")
+        vscrollbar.grid(row=1,column=1,sticky="ns")
         # vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
         self.canvas = tk.Canvas(self, bd=0, highlightthickness=0,
                                 width = 200, height = 300,
                                 yscrollcommand=vscrollbar.set)
-        self.canvas.grid(row=0,column=0,sticky="nswe")
+        self.canvas.grid(row=1,column=0,sticky="nswe")
         # self.canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
         vscrollbar.config(command = self.canvas.yview)
-        self.okbutton = ttk.Button(self,text="Save",command=self.ok_pressed)
-        self.okbutton.grid(row=1,column=0,columnspan=2,sticky="we",pady=1,padx=1)
 
         # Reset the view
         self.canvas.xview_moveto(0)
@@ -114,7 +119,7 @@ class VerticalScrolledFrame(ttk.Frame): # https://coderslegacy.com/python/make-s
         # Create a frame inside the canvas which will be scrolled with it.
         self.interior       = ttk.Frame(self.canvas)
 
-        self.interior.bind  ('<Configure>',   self._configure_interior)
+        self.interior.bind  ('<Configure>',     self._configure_interior)
         self.canvas.bind    ('<Configure>',     self._configure_canvas)
 
         self.interior_id = self.canvas.create_window(   0, 0,
@@ -132,12 +137,11 @@ class VerticalScrolledFrame(ttk.Frame): # https://coderslegacy.com/python/make-s
     #         self._scroll_down()
     #         return "break"
 
-    def ok_pressed(self):
-        self.event_generate('<<OkPressed>>')
 
     def _configure_interior(self, event):
         # Update the scrollbars to match the size of the inner frame.
         size = (self.interior.winfo_reqwidth(), self.interior.winfo_reqheight())
+        # print("NEW Canvas size",size)
         self.canvas.config( scrollregion=(  0, 0,
                                             size[0], size[1] ));
         if self.interior.winfo_reqwidth() != self.canvas.winfo_width():
@@ -167,66 +171,135 @@ class Window():
     );
 
     def __init__(self, root, *args, **kwargs):
-        self.root           = root
-        self.root.protocol  ( "WM_DELETE_WINDOW", self.quit );
-        self.root.geometry  ("600x400")
-        self.root.minsize   (400,200)
+        self.root                   = root
+        self.root.protocol          ( "WM_DELETE_WINDOW", self.quit );
+        self.root.geometry          ("600x400")
+        self.root.minsize           (400,200)
+        self.big_font_size          = 10
+        self.small_font_size        = 9
+        self.zooming                = False
 
+        self.startup                = True
+        self.frame                  = VerticalScrolledFrame (   root );
+        self.desktop_boxes          = []
 
-        self.startup        = True
-        self.frame          = VerticalScrolledFrame (   root );
-        self.desktop_boxes  = []
-        self.frame.pack     (   expand = True, fill = tk.BOTH );
+        self.okbutton               = ttk.Button(self.root,text="Save",command=self.ok_pressed)
+        self.okbutton.pack          (   expand = False, fill = "x" );
+        self.frame.pack             (   expand = True, fill = tk.BOTH );
 
         self.passed_file_item       = None
         self.passed_file_item_ref   = None
         self.passed_file_path       = None
         self.ctrl_c_timer           = None
 
-        self.frame.bind("<<OkPressed>>",self.ok_pressed)
+        self.root.bind("<<OkPressed>>",self.ok_pressed)
         self.root.bind("<Control-plus>",self.zoom_in)
+        self.root.bind("<Control-minus>",self.zoom_out)
         self.root.bind("<Return>",self.ok_pressed)
 
-    def zoom_in(self,e):
-        print("zooming1")
-        self.root.event_generate('<<ZoomIn>>')
+    def ok_pressed(self):
+        self.event_generate('<<OkPressed>>')
+
+    def update_fontsize(self,new_size1,new_size2):
+        if self.zooming == True:
+            for parent in self.frame.interior.winfo_children():
+
+                for child in parent.winfo_children():
+
+                    for grandchild in child.winfo_children():
+
+                        if isinstance(grandchild,ttk.Label):
+
+                            if grandchild.tag == "s":
+                                grandchild.configure(font=("",new_size1))
+
+                            elif grandchild.tag == "k":
+                                grandchild.configure(font=("",new_size2))
+
+                        elif isinstance(grandchild,TweakedEntry):
+                            grandchild.configure(font=("",new_size2))
+
+            self.root.update_idletasks()
+            self.root.after(20,self.reset_zoom)
+
+    def reset_zoom(self):
+        self.zooming = False
+
+    def zoom_out(self,e=None): # has caused crashing
+        if self.small_font_size > 8 and self.zooming == False:
+            self.zooming = True
+            self.big_font_size      -= 1
+            self.small_font_size    -= 1
+
+            self.update_fontsize    (   self.big_font_size,
+                                        self.small_font_size );
+
+    def zoom_in(self,e=None):
+        if self.small_font_size < 13 and self.zooming == False:
+            self.zooming = True
+            self.big_font_size      += 1
+            self.small_font_size    += 1
+            self.update_fontsize    (   self.big_font_size,
+                                        self.small_font_size );
+
+
+        # self.root.event_generate('<<ZoomIn>>')
 
     def quit(self):
 
         if self.ctrl_c_timer != None:
+
             self.root.after_cancel          (self.ctrl_c_timer)
             self.ctrl_c_timer               = None
+
         self.root.destroy                   ()
 
     def ok_pressed(self,event=None):
         if self.passed_file_item_ref != self.passed_file_item:
+
+# --- ### SAVE THE FILE ### -------------------------------------------------------------------------- BEGIN
+
+            did_save = False
             try: # https://docs.python.org/3/glossary.html#term-EAFP - like good god damned snakes
-                conf = configparser.RawConfigParser()
-                conf.optionxform = str
+                conf                = configparser.RawConfigParser()
+                conf.optionxform    = str
                 for i,x in self.passed_file_item.items():
-                    conf[i]=x
+                    conf[i]         = x
                 with open(self.passed_file_path, 'w+', encoding='utf8') as configfile:   # not sure about the encoding
-                    conf.write(configfile,space_around_delimiters=False)
+                    conf.write      (configfile,space_around_delimiters=False)
 
             except Exception as e:
 
-                error_msg = f"Error occured while saving file:\n{self.passed_file_path}\n\n{e}"
-                print(error_msg,e)
-                tk.messagebox.showerror(title="Desktop-file editor:",
-                                        message=error_msg)
+                error_msg               =   f"Error occured while saving file:\n{self.passed_file_path}\n\n{e}"
+                print                   (   error_msg,  e   )
+                messagebox.showerror (   title="Desktop-file editor:",
+                                            message=error_msg
+                );
                 self.root.destroy()
+            else:
+                did_save = True
 
-            new_passed_file_item = {}
+# --- ### SAVE THE FILE ### ---------------------------------------------------------------------------- END
+
+            new_passed_file_item                            = {}
 
             for section in conf.sections():
-                new_passed_file_item [section]                = {}
+                new_passed_file_item [section]              = {}
 
                 for key, val in conf.items    (section):
-                    new_passed_file_item [section][key]       = val
+                    new_passed_file_item [section][key]     = val
 
-            self.passed_file_item = new_passed_file_item
-            self.passed_file_item_ref = deepcopy(self.passed_file_item)
+            self.passed_file_item                           = new_passed_file_item
+            self.passed_file_item_ref                       = deepcopy(self.passed_file_item)
             self.check_save_enabled()
+            if did_save:
+                result = messagebox.askquestion(title=f"Saved document", message=f"{basename(self.passed_file_path)} was saved\nDo you want to open the containing folder?\n\n{dirname(self.passed_file_path)}")
+
+                if result == 'yes':
+                    args = ["xdg-open",dirname(self.passed_file_path)]
+                    subprocess_call(args, stderr=DEVNULL, stdout=DEVNULL)
+        else:
+            print(f"Not saving: {basename(self.passed_file_path)} is unchanged.")
 
 
 
@@ -239,16 +312,16 @@ class Window():
                 # print(i,x)
 
                 box                 =   tk.Frame    (   subframe );
-                top_label           =   tk.Label    (   box, text=i, font=("",10) );
+                top_label           =   ttk.Label    (   box, text=i, font=("",self.big_font_size) );
+                top_label.tag       =   "s"
                 top_label.grid      (   row=0,
                                         column=0, columnspan=2 );
                 b_row = 1
 
                 for a,b in x.items():
                     # print(a,"=",b)
-                    label               =   tk.Label        (   box,text=a, anchor="w",font=("",10) );
-                    label.bind('<<ZoomIn>>',lambda e:print("zooming"))
-
+                    label               =   ttk.Label        (   box,text=a, anchor="w",font=("",self.small_font_size) );
+                    label.tag           =   "k"
                     label.grid          (   row=b_row,
                                             column=0,
                                             sticky="we" );
@@ -256,7 +329,7 @@ class Window():
                     entry               =   TweakedEntry    (   parent=box,
                                                                 root=self.root,
                                                                 # key=a,
-                                                                font=("",10)
+                                                                font=("",self.small_font_size)
                     );
                     # '<<ReturnPressed>>' is hmmmm...
                     entry.bind('<<FieldChanged>>', lambda e, key=a, section=i: \
@@ -284,21 +357,22 @@ class Window():
             self.subframe = subframe
 
         else:
+            print("something is very wrong. sorry")
             self.root.destroy()
 
-        self.passed_file_item_ref = deepcopy(self.passed_file_item)
-        self.startup = False
-        self.check_save_enabled()
-        self.root.title(f"Editing: {self.passed_file_path}")
-        self.toggleCtrlCTimer()
+        self.passed_file_item_ref   = deepcopy(self.passed_file_item)
+        self.startup                = False
+        self.check_save_enabled     ()
+        self.root.title             (f"Editing: {self.passed_file_path}")
+        self.toggleCtrlCTimer       ()
 
 
     # def return_pressed(self,e,key,section):
     def check_save_enabled(self):
         if self.passed_file_item_ref == self.passed_file_item:
-            self.frame.okbutton.configure(state="disabled")
+            self.okbutton.configure(state="disabled")
         else:
-            self.frame.okbutton.configure(state="normal")
+            self.okbutton.configure(state="normal")
 
     def text_changed(self,e,key,section):
         if self.startup == False:
