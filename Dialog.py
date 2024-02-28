cff451032cce41f88963c6d400a34a839188f07b
Skrimpton <64572787+Skrimpton@users.noreply.github.com>
2024-02-28 16:53:19 +0100

    Update Dialog.py

--- Dialog.py
+++ Dialog.py
@@ -1,5 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
+
+from os.path import (   isfile,
+                        dirname,
+                        basename,
+                        realpath
+)
+from os import      (   access,
+                        W_OK
+)
+
+from TweakedEntry   import CEntry as TweakedEntry
 class QuestionDialog(tk.Toplevel):
 
     def __init__(self, parent=None, saved_file=None , root=None, text="",  *args, **kwargs):
@@ -31,7 +42,7 @@ class QuestionDialog(tk.Toplevel):
 
         self.question.bind('<Configure>', lambda e: self.question.config(wraplength=self.info.winfo_width()))
 
-        self.question.pack  (expand=0,   side="top",    fill=BOTH,   anchor="n")
+        self.question.pack  (expand=0,   side="top",    fill="both", anchor="n")
         self.info.pack      (expand=0,   side="top",    fill="x",    anchor="n")
         self.yes.pack       (expand=1,   side="left",   fill="x",    anchor="s")
         self.no.pack        (expand=1,   side="right",  fill="x",    anchor="s")
